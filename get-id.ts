59dd6f3170e640b2e3d69956c7e7c3cc2032a091
Micha <klokhuiswertas@gmail.com>
2024-10-14 22:17:21 +0000

    also add it on get-id.ts

--- get-id.ts
+++ get-id.ts
@@ -19,10 +19,17 @@ async function main() {
     name: process.env.SCHOOL,
   });
   if (!org) throw new Error("School not found");
-  const user = await org.authenticate({
-    username: process.env.USERNAME,
-    password: process.env.PASSWORD,
-  });
+  let user;
+  if (process.env.METHOD == "token") {
+    user = await org.authenticate(fs.readFileSync("refresh_token", "utf-8").trim());
+    await user.authenticated;
+    fs.writeFileSync("refresh_token", user.refreshToken);  
+  } else {
+    user = await org.authenticate({
+      username: process.env.USERNAME,
+      password: process.env.PASSWORD,
+    });
+  }
 	const students = await user.getStudents();
 	console.log("Add this to your .env file: ID=" + students[0].id)
 }
